9d28922b8b898f982cc2aebb9c10307fd45d2deb
Esteban Ramirez <estebanraul313@gmail.com>
2024-10-25 18:29:51 -0500

    modificaciones para db

--- src/main/java/com/focused/gestor_tareas/controller/UsuarioController.java
+++ src/main/java/com/focused/gestor_tareas/controller/UsuarioController.java
@@ -5,7 +5,6 @@ import com.focused.gestor_tareas.service.UsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -17,36 +16,7 @@ public class UsuarioController {
 
     @GetMapping
     public List<Usuario> listarUsuarios() {
-        List<Usuario> usuarios = new ArrayList<>();
-
-        Usuario usuario1 = new Usuario();
-        usuario1.setId(1L);
-        usuario1.setNombre("Juan");
-        usuario1.setRol("admin");
-        usuario1.setTelefono("3135042682");
-        usuario1.setCorreo("correo@gmail.com");
-        usuario1.setContraseña("pedrito");
-        usuarios.add(usuario1);
-
-        Usuario usuario2 = new Usuario();
-        usuario2.setId(1L);
-        usuario2.setNombre("Pedro");
-        usuario2.setRol("miembro");
-        usuario2.setTelefono("3325236233");
-        usuario2.setCorreo("correo2@gmail.com");
-        usuario2.setContraseña("camas");
-        usuarios.add(usuario2);
-
-        Usuario usuario3 = new Usuario();
-        usuario3.setId(1L);
-        usuario3.setNombre("Maria");
-        usuario3.setRol("miembro");
-        usuario3.setTelefono("31353552682");
-        usuario3.setCorreo("correo3@gmail.com");
-        usuario3.setContraseña("mari");
-        usuarios.add(usuario3);
-
-        return usuarios;
+        return usuarioService.listarUsuarios();
     }
 
     @GetMapping("/{id}")
